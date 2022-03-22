2fb23d99da2639e07858be5bd17118b1465a5cc7
Devin Campana <devin.campana@shp.org>
2022-03-22 14:28:33 -0400

    Initial Commit

---
+++ github Demo/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  github Demo
+//
+//  Created by Devin Campana on 3/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+//comments so a change can show
+    
+    struct something {
+        var thing1: String
+        var thing2: String
+        var thing3: Int
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
